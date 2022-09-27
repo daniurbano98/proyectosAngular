b3f732a5a7278d571f4e0606fb22d3c81ceada6a
Dani Urbano <daniurbanogarcia26@gmail.com>
2022-09-27 17:34:29 +0200

    seguimos pues

---
+++ src/app/components/saludo/saludo.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-saludo',
+  templateUrl: './saludo.component.html',
+  styleUrls: ['./saludo.component.css']
+})
+export class SaludoComponent implements OnInit {
+
+  nombre: string = "Martin";
+
+  constructor() { }
+
+  ngOnInit(): void {
+    // isntrucciones previas a la renderizacion del componente
+    console.log("ngOnInit del componente saludo")
+  }
+
+}
